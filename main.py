ba74d319a755b4307462b8645284bd421e11b57b
Jicotemx <o.davalos.o@gmail.com>
2025-07-07 15:28:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ MinutoEmpieza=31
 minutes=HoraEmpieza*60+MinutoEmpieza
 #START_TIME = datetime.now()#.replace(second=0, microsecond=0) + timedelta(minutes)  # Empieza en 1 minuto desde que corre
 #START_TIME = datetime(2025,07,07,15,10)
-START_TIME = datetime(year=2025, month=7, day=7, hour=15, minute=20, second=0, microsecond=0)
+#START_TIME = datetime(year=2025, month=7, day=7, hour=15, minute=20, second=0, microsecond=0)
 DURATION = timedelta(minutes=20)  # Duración del concurso
 LOCAL_TIMEZONE = pytz.timezone("America/Mexico_City")  # Cambia según tu ubicación
-
+START_TIME = LOCAL_TIMEZONE.localize(datetime(2025, 1, 2, 16, 35, 0, 0))
 # Lista de problemas
 problems = {
     "A": 42,

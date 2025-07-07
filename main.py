ba696dc76b7bd7c0dd218860e8525cec4cca75c7
Jicotemx <o.davalos.o@gmail.com>
2025-07-07 17:34:20 -0600

    Update main.py

--- main.py
+++ main.py
@@ -30,9 +30,7 @@ problems = {
 
 # Almacén de resultados y participantes
 participants = {}  # nombre -> info
-@app.route("/participants")
-def get_participants():
-    return jsonify(list(participants.keys()))
+
 
 
 # =====================

71e163ebcd83ddf130cd90d1e9babc4e673cd216
Jicotemx <o.davalos.o@gmail.com>
2025-07-07 15:05:47 -0600

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ socketio = SocketIO(app)
 HoraEmpieza=13
 MinutoEmpieza=31
 minutes=HoraEmpieza*60+MinutoEmpieza
-START_TIME = datetime.now()#.replace(second=0, microsecond=0) + timedelta(minutes)  # Empieza en 1 minuto desde que corre
+#START_TIME = datetime.now()#.replace(second=0, microsecond=0) + timedelta(minutes)  # Empieza en 1 minuto desde que corre
+START_TIME = datetime(2025,07,07,15,10)
 DURATION = timedelta(minutes=20)  # Duración del concurso
 LOCAL_TIMEZONE = pytz.timezone("America/Mexico_City")  # Cambia según tu ubicación
 

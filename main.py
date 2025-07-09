22bb47b5d7b96c09c1854ad784f04f32bff1e4e8
Jicotemx <o.davalos.o@gmail.com>
2025-07-09 08:57:42 -0600

    Update main.py

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ def get_status():
     if now < START_TIME:
         return "before"
     elif now > START_TIME + DURATION:
-        if now >= startTime + timedelta(seconds=durationSeconds) and not informe_subido:
+        if informe_subido:
            guardar_y_subir_informe(participantes, startTime)
            informe_subido = True
         return "after"

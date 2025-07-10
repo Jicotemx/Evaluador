53c67b6b16180fa74c7ffa8fe724bb5881455412
Jicotemx <o.davalos.o@gmail.com>
2025-07-10 11:35:41 -0600

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ socketio = SocketIO(app)
 # =====================
 # CONFIGURACIÓN
 # =====================
-anno=2025; dia=10; mes=7;  hora=8; minuto=52
+anno=2025; dia=10; mes=7;  hora=11; minuto=37
 duracion=3
 DURATION = timedelta(minutes=duracion)  # Duración del concurso
 LOCAL_TIMEZONE = pytz.timezone("America/Mexico_City")  # Cambia según tu ubicación
